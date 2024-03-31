a6ea6f7b378de1d3377376da466f180d8b6c6506
Blackie360 <felixkent360@gmail.com>
2024-03-31 03:36:52 +0300

    setting up meeting : camera & mic

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -17,7 +17,7 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast className="border-none bg-slate-200" key={id} {...props}>
+          <Toast className="border-none bg-slate-200 shadow-red-600" key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (

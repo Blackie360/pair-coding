4b3dbb56a37c05a7ddc941fab3565d47d7908742
blackie360 <felixkent360@gmail.com>
2024-08-24 14:30:51 +0300

    sketched the sign up form

---
+++ app/(pages)/signup/page.tsx
@@ -0,0 +1,20 @@
+import Signupform from '@/components/Signupform'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Card className="max-w-md w-full p-4 bg-white shadow-md rounded-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold text-center">Sign Up</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Signupform />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default Signup
